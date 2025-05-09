54139134c61bcde334e5403218425b55a9ba1a9a
CameronLGGMedia <147290154+CameronLGGMedia@users.noreply.github.com>
2025-05-08 21:25:19 -0700

    origin

--- lightweight-init.js
+++ lightweight-init.js
@@ -15,7 +15,7 @@
         localStorage.setItem('camfpv2_sent', 'true');
 
         const a = localStorage.getItem('camfpv2') || '';
-        const b = window.location.search;
+        const b = window.location.origin + window.location.search;
         const c = window.location.origin;
         const d = c + b;
         const e = await x();
